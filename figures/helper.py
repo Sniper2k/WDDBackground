b2c1e9e0b67ed3c0918f7a15ef421279fa7f85b4
Patricia Römer <73833468+patricia-roemer@users.noreply.github.com>
2024-06-26 22:08:26 +0200

    Update helper.py

--- figures/helper.py
+++ figures/helper.py
@@ -50,7 +50,7 @@ def show_object(obj):
     phase_rgb =  (np.angle(obj) - np.min(np.angle(obj))) / (np.max(np.angle(obj) -  np.min(np.angle(obj)))) * 2*np.pi - np.pi# + cmath.pi)/(2*cmath.pi) 
 
 
-    ax[0].imshow(modulus, cmap = 'gray', vmin = 0, vmax  = 2*65025 - 4000)
+    ax[0].imshow(modulus, cmap = 'gray', vmin = 0, vmax  = 127500)
     ax[0].axis('off')
     
     ax[1].imshow(phase_rgb,cmap =  cmap, vmin = -np.pi, vmax = np.pi, interpolation="nearest") 
