9e1e62d6eb048a909ff1dedb9bad31c254c8f033
Patricia Römer <73833468+patricia-roemer@users.noreply.github.com>
2024-06-27 14:04:31 +0200

    Update helper.py

--- figures/helper.py
+++ figures/helper.py
@@ -38,6 +38,8 @@ def image_to_phase_object(im,satur_parser):
 def show_object(obj):
     colors = ["black", "lightgray", "black"]
     cmap = LinearSegmentedColormap.from_list("", colors)
+    colors = ["black", "gray", "lightgray", "white"]
+    cmap2 = LinearSegmentedColormap.from_list("", colors)
     
     fig, ax = plt.subplots(nrows = 1, ncols = 2)
     
@@ -49,8 +51,7 @@ def show_object(obj):
     
     phase_rgb =  (np.angle(obj) - np.min(np.angle(obj))) / (np.max(np.angle(obj) -  np.min(np.angle(obj)))) * 2*np.pi - np.pi# + cmath.pi)/(2*cmath.pi) 
 
-
-    ax[0].imshow(modulus, cmap = 'gray', vmin = 0, vmax  = 127500)
+    ax[0].imshow(modulus, cmap = cmap2, vmin = 0, vmax  = 135000)
     ax[0].axis('off')
     
     ax[1].imshow(phase_rgb,cmap =  cmap, vmin = -np.pi, vmax = np.pi, interpolation="nearest") 
